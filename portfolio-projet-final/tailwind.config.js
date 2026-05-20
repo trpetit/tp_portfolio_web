f0a3188bcd9614fbbb410e849502604548d3796f
TristanPETIT <tristanpetit33@gmail.com>
2026-05-20 15:51:46 +0200

    Ajout du fichier tailwin.config

---
+++ portfolio-projet-final/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",],
+  theme: {
+    extend: {
+        colors: {
+          portfolio: {
+          dark: '#25282B',      
+          yellow: '#FDC435',    
+          gray: '#828282',      
+          lightBg: '#F9FAFF',  
+          muted: '#9A9FA8',
+    },
+  },
+  fontFamily: {
+    'sans': ['Raleway', 'system-ui', 'sans-serif'],
+  },
+},
+  plugins: [],
+}
+
