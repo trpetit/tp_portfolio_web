e18763f8cef19c2a5955238029a927911cb8a014
TristanPETIT <tristanpetit33@gmail.com>
2026-05-20 16:34:46 +0200

    Ajout footer

--- portfolio-projet-final/src/components/Footer.jsx
+++ portfolio-projet-final/src/components/Footer.jsx
@@ -1,10 +1,19 @@
-function Footer() {
+import footerIcons from '../assets/footer_icons.svg';
+
+function Footer({name}) {
   return (
     <footer className="bg-white py-8 border-t border-brand-muted/20">
       <div className="container mx-auto px-4 text-center">
         {}
-        <p className="text-brand-gray">[Composant Footer]</p>
-      </div>
+        <img 
+          src={footerIcons} 
+          alt="Icônes de réseaux sociaux" 
+          className="mx-auto mt-4" 
+        />      
+        <p className="text-brand-gray text-sm font-medium tracking-wide">
+          {name}  2026
+          </p>
+        </div>
     </footer>
   );
 }
