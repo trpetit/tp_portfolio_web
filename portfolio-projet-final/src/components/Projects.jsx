cc660e2c0f4ada36f063fd1ee4f80f8caba4c314
TristanPETIT <tristanpetit33@gmail.com>
2026-05-22 16:38:09 +0200

    Ajout de la page des projets

--- portfolio-projet-final/src/components/Projects.jsx
+++ portfolio-projet-final/src/components/Projects.jsx
@@ -1,10 +1,38 @@
+import CookieImage from '../assets/Cookie.png';
+import CitronImage from '../assets/Citron.png';
+import TarteImage from '../assets/Tarte.png';
+import SingleProject from './SingleProject';
+
+
 function Projects() {
+
+const projectsList = [
+    { id: 1, title: "Cookies fondants", description: "Des délicieux cookies aux pépites de chocolat", image: CookieImage },
+    { id: 2, title: "Tarte au citron meringuée", description: "Une tarte sucrée et rafraichissante", image: CitronImage },
+    { id: 3, title: "Tarte chocolat caramel", description: "Une tarte au chocolat gourmande", image: TarteImage }
+  ];
+
+
+
   return (
-    <section className="bg-white py-12">
-      <div className="container mx-auto px-4">
-        {}
-        <p className="text-brand-dark font-bold">[Composant Projects]</p>
-      </div>
+  
+    <section className=" py-12 max-w-4xl mx-auto">
+    <h3 className="mb-8 text-center font-elegant font-bold text-5xl">Projects</h3>
+    <span className=" block  bg-portfolio-yellow rounded-xl p-[0.1em] w-[4em] mx-auto">
+    
+    </span>
+      {projectsList.map((projects, index) => (
+        <SingleProject
+          key={projects.id}
+          title={projects.title}
+          description={projects.description}
+          image={projects.image}
+          index={index}
+          className="my-8"
+        />
+      ))}
+    <h3 className="mb-8 text-center font-elegant font-bold text-5xl">Projects</h3>
+    <span className=" block  bg-portfolio-yellow rounded-xl p-[0.1em] w-[4em] mx-auto"></span>
     </section>
   );
 }
--- portfolio-projet-final/src/components/SingleProject.jsx
+++ portfolio-projet-final/src/components/SingleProject.jsx
@@ -1,13 +1,24 @@
-function SingleProject({ title, description, image }) {
-  return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden">
-      <img src={image} alt={title} className="w-full h-48 object-cover" />
-      <div className="p-4">
-        <h3 className="text-xl font-semibold mb-2">{title}</h3>
-        <p className="text-gray-600">{description}</p>
-      </div>
-    </div>
-  );
+import BoutonRecette from "./BoutonRecette";
+
+function SingleProject({ title, description, image, index, className }) {
+
+    const isEven = index % 2 === 0;
+
+
+    return (
+        <div className={`flex items-center bg-white rounded-2xl shadow-md overflow-hidden w-full ${isEven ? 'flex-row' : 'flex-row-reverse'} ${className || ''}`}>
+            <div className="p-8 w-1/2 flex center flex-col gap-4">
+                <h3 className="text-xl font-semibold mb-2">{title}</h3>
+                <p className="text-gray-600">{description}</p>
+                <BoutonRecette className="w-[50%]"/>
+            </div>
+
+            <div className="w-1/2">
+                <img src={image} alt={title} className="w-full h-48 object-cover" />
+            </div>
+
+        </div>
+    );
 }
 
 export default SingleProject;
