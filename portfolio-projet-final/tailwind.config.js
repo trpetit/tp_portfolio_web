ddb670fe1483f8b84f0b5d9062a3b1fedc92577c
TristanPETIT <tristanpetit33@gmail.com>
2026-05-22 16:37:21 +0200

    MAJ fonts + ajout des images

--- portfolio-projet-final/tailwind.config.js
+++ portfolio-projet-final/tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
-        colors: {
-          portfolio: {
-          dark: '#25282B',      
-          yellow: '#FDC435',    
-          gray: '#828282',      
-          lightBg: '#F9FAFF',  
+      colors: {
+        portfolio: {
+          dark: '#25282B',
+          yellow: '#FDC435',
+          gray: '#828282',
+          lightBg: '#F9FAFF',
           muted: '#9A9FA8',
+        },
+      },
+      fontFamily: {
+        'sans': ['Raleway', 'system-ui', 'sans-serif'],
+        'elegant': ['"Playfair Display"', 'serif'],
+      },
     },
   },
-  fontFamily: {
-    'sans': ['Raleway', 'system-ui', 'sans-serif'],
-  },
-},
-},
   plugins: [],
 }
